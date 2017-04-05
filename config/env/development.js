dfd4d49a5f3fbac428ec3e9165ddbaca190e710e
piyapan <subaruqui@gmail.com>
2017-04-05 13:20:15 +0700

    Update development.js

--- config/env/development.js
+++ config/env/development.js
@@ -3,13 +3,13 @@ module.exports = {
 	mongoUri : 'mongodb://localhost/tweet',
 	sessionSecret: 'dev_secret_key',
 	facebook:{
-		clientID: '1065348796855482',
-		clientSecret: '25449343e94b9597a020a81981df1b48',
+		clientID: '#',
+		clientSecret: '#',
 		callbackURL: 'http://localhost:3000/oauth/facebook/callback'
 	},
 	google:{
-		clientID: '375902756284-6ejh17375qjj8kf5aimlnns9amt90cod.apps.googleusercontent.com',
-		clientSecret: 'D58JOx25nbJbzWgOWHda89ly',
+		clientID: '#',
+		clientSecret: '#',
 		callbackURL: 'http://localhost:3000/oauth/google/callback'
 	}
 };
